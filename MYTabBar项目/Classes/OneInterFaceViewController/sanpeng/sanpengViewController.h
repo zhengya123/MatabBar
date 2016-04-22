59b43762586562b45b2915a84acc9d2460017936
zhengya <15210093835@163.com>
2016-04-22 10:26:48 +0800

    no message

---
+++ MYTabBar项目/Classes/OneInterFaceViewController/sanpeng/sanpengViewController.h
@@ -0,0 +1,13 @@
+//
+//  sanpengViewController.h
+//  MYTabBar项目
+//
+//  Created by dqong on 16/4/22.
+//  Copyright © 2016年 dqong. All rights reserved.
+//
+
+#import "TWOBASEViewController.h"
+
+@interface sanpengViewController : TWOBASEViewController
+
+@end
---
+++ MYTabBar项目/Classes/OneInterFaceViewController/sanpeng/sanpengViewController.m
@@ -0,0 +1,37 @@
+//
+//  sanpengViewController.m
+//  MYTabBar项目
+//
+//  Created by dqong on 16/4/22.
+//  Copyright © 2016年 dqong. All rights reserved.
+//
+
+#import "sanpengViewController.h"
+
+@interface sanpengViewController ()
+
+@end
+
+@implementation sanpengViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
